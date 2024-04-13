153d83011d658c7b35a851b99e7aa2acff12bda0
Tommyyoliver <tommyolivr@gmail.com>
2024-04-13 16:51:22 -0300

    err

---
+++ src/app/client/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  DATABASE_URL: "libsql://from-your-home-to-the-world-tommyyoliver.turso.io",
+  DATABASE_AUTH_TOKEN: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTI5NTE4ODEsImlkIjoiMDM3ZjlmZTMtOThjZi00MTY1LWJjOWEtNmUyNGZjNDBiODUxIn0.7qafj0HDVilOiDHODzlFwf6U5QPbB7sGbB5evDC9AUxL4i5cxfVPfefoaS3jqTPcZaRjj764iePuJujFyVoSAw"
+}
